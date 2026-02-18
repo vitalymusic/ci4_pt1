3195460297ca68e09e7f353e99339465bef0daef
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-02-18 18:04:58 +0200

    ci4

--- app/Views/main_layout.php
+++ app/Views/main_layout.php
@@ -23,7 +23,7 @@
 
 
     <footer>
-        <p class="text-light bg-dark p-5">This is footer</p>
+        <p class="text-light bg-dark p-5 text-center">This is footer</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
