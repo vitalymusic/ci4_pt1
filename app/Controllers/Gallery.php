a841ee9d9e40548c7df65086335a9a6201e41726
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-02-25 21:23:23 +0200

    gallery upload

--- app/Controllers/Gallery.php
+++ app/Controllers/Gallery.php
@@ -5,6 +5,11 @@ namespace App\Controllers;
 class Gallery extends BaseController
 {
 
+    public function __construct(){
+        $this->db = \Config\Database::connect();
+
+    }
+
 public function index(){
     // Parādīt visu galeriju
     return view('admin/gallery_index',['title'=>"Bilžu galerija"]);
@@ -20,9 +25,21 @@ public function uploadForm(){
 
 public function uploadPhoto(){
         $img = $this->request->getFile('file');
-        $filepath = FCPATH . 'uploads/' . $img->store();
+        $img->move(FCPATH . 'uploads', $img->getName());
+        
+         $data = [
+               'url'=> $img->getName()
+           ]; 
+           try{
+                $builder = $this->db->table('images');
+                $query   = $builder
+                     ->insert($data); 
+                return $this->response->setJSON(["page_save"=>"success"]);
+           }
+           catch(Error){
+                return $this->response->setJSON(["error"=>Error]);
+           }
 
-        dd($filepath);
         
 
 }
